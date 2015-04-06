ed3fa295751a8cc7c3c4b4c14ae4e97c4d81c08f
Nickolas Lapp <nickolaslapp@gmail.com>
2015-04-05 18:41:56 -0600

    Output matches that provided.

--- FloydWarshall.java
+++ FloydWarshall.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class FloydWarshall {
     public int adjacencyMatrix[][];
     public static final int INFINITY = Integer.MAX_VALUE / 2;
@@ -13,40 +17,61 @@ public class FloydWarshall {
                     if (adjacencyMatrix[iteration][j] + adjacencyMatrix[i][iteration] < adjacencyMatrix[i][j]) {
                         adjacencyMatrix[i][j] = adjacencyMatrix[iteration][j] + adjacencyMatrix[i][iteration];
                         printAdjacencyMatrix();
+                        if (iteration < adjacencyMatrix[0].length)
+                            getInput();
                     }
         }
     }
 
+    public String getInput() {
+        System.out.println("Press enter to continue:");
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        String s = null;
+        try {
+            s = br.readLine();
+        } catch (IOException e) {
+            System.out.println("Unable to read input. Please retry.");
+        }
+        return s;
+    }
+
     public void printAdjacencyMatrix() {
+
+        System.out.print("  ");
         for (int i = 0; i < adjacencyMatrix.length; i++) {
-            System.out.print((char) (i + (int) 'A') + " ");
+            System.out.print("   " + (char) (i + (int) 'A') + " ");
         }
         System.out.println();
+        System.out.print("  ");
         for (int i = 0; i < adjacencyMatrix.length; i++) {
-            System.out.print("==");
+            System.out.print("=====");
         }
         System.out.println();
 
         for (int row = 0; row < adjacencyMatrix.length; row++) {
-            System.out.print((char) (row + (int) 'A') + " ");
+            System.out.print((char) (row + (int) 'A') + " |");
             for (int col = 0; col < adjacencyMatrix.length; col++)
                 if (adjacencyMatrix[row][col] != INFINITY)
-                    System.out.print(String.format("% 3d ", adjacencyMatrix[row][col]));
+                    System.out.print(String.format("% 4d ", adjacencyMatrix[row][col]));
                 else
-                    System.out.print("Inf ");
+                    System.out.print("---- ");
             System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        int testMatrix[][] = { { 0, 50, FloydWarshall.INFINITY, 80, FloydWarshall.INFINITY },
-                { FloydWarshall.INFINITY, 0, 60, 90, FloydWarshall.INFINITY },
-                { FloydWarshall.INFINITY, FloydWarshall.INFINITY, 0, FloydWarshall.INFINITY, 40 },
-                { FloydWarshall.INFINITY, FloydWarshall.INFINITY, 20, 0, 70 },
-                { FloydWarshall.INFINITY, 50, FloydWarshall.INFINITY, FloydWarshall.INFINITY, 0 } };
+        int testMatrix[][] = { { FloydWarshall.INFINITY, 50, FloydWarshall.INFINITY, 80, FloydWarshall.INFINITY },
+                { FloydWarshall.INFINITY, FloydWarshall.INFINITY, 60, 90, FloydWarshall.INFINITY },
+                { FloydWarshall.INFINITY, FloydWarshall.INFINITY, FloydWarshall.INFINITY, FloydWarshall.INFINITY, 40 },
+                { FloydWarshall.INFINITY, FloydWarshall.INFINITY, 20, FloydWarshall.INFINITY, 70 },
+                { FloydWarshall.INFINITY, 50, FloydWarshall.INFINITY, FloydWarshall.INFINITY, FloydWarshall.INFINITY } };
         FloydWarshall testWarshall = new FloydWarshall(testMatrix);
 
-        for (int i = 0; i < testMatrix.length; i++)
-            testWarshall.processArray(i);
+        System.out.println("Adjacency Matrix:");
+        testWarshall.printAdjacencyMatrix();
+
+        System.out.println("All Pairs Shortest Paths:");
+        testWarshall.processArray();
     }
 }
